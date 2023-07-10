041b133827c1f52751912c13da3d8db626e05b9e
MinyTheMiner <125713973+MinyTheMiner@users.noreply.github.com>
2023-07-10 12:25:26 +1200

    Add files via upload

---
+++ html test/script.js
@@ -0,0 +1,15 @@
+const Input = document.getElementById('inputs');
+const Div = document.getElementById('divs');
+
+window.onload = () => {
+	Div.innerHTML = localStorage.getItem('htmls');
+	Input.value = localStorage.getItem('input');
+};
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.key);
+    if (e.key !== 'aa') {
+	Div.innerHTML = Input.value;
+	localStorage.setItem('htmls', Div.innerHTML);
+	localStorage.setItem('input', Input.value);
+}});
